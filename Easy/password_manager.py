f0b14d47696faf9e5ecbf8d38db09b2707248d28
Aditya Pandey <103255068+pandeyaditya0002@users.noreply.github.com>
2025-02-19 18:20:32 +0530

    started implementation with code

---
+++ Easy/password_manager.py
@@ -0,0 +1,19 @@
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
