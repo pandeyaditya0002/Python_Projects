6e49e840289b4dda65584510bcb7259ecf381449
Aditya Pandey <103255068+pandeyaditya0002@users.noreply.github.com>
2025-02-24 16:23:47 +0530

    .

--- Medium/slot_machine.py
+++ Medium/slot_machine.py
@@ -2,7 +2,9 @@
 
 def deposit():
     while True:
-        amount =int(input("What would you like to deposite ?"))
+        amount = input("What would you like to deposite ?")
         if amount.isdigit():
             amount = int(amount)
+            if amount > 0 :
+                break
 
