24c456ce3fbedce54102cce2829f3503e2662c3b
Aditya Pandey <103255068+pandeyaditya0002@users.noreply.github.com>
2025-02-24 15:25:57 +0530

    ..

--- Medium/Timed_math_challenge.py
+++ Medium/Timed_math_challenge.py
@@ -5,4 +5,13 @@ import random
 OPERATORS = ["+","-","/","*"]
 MIN_OPRAND = 3
 MAX_OPERAND = 12
+
+
 def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer
