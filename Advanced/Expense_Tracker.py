5888f027e88f3bb3bb5578ba6a976b31974a7829
Aditya Pandey <103255068+pandeyaditya0002@users.noreply.github.com>
2025-02-28 12:12:30 +0530

    ..

--- Advanced/Expense_Tracker.py
+++ Advanced/Expense_Tracker.py
@@ -11,3 +11,10 @@ def load_expenses():
 def save_expenses(expenses):
     with open("expenses.json", "w") as file:
         json.dump(expenses, file, indent=4)
+
+        
+def add_expense(amount, category, description):
+    expenses = load_expenses()
+    expenses.append({"amount": amount, "category": category, "description": description})
+    save_expenses(expenses)
+    print("Expense added successfully!")
