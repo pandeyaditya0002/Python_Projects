a9a9adb8ed65f8501793609c5a8e429a5ff3090d
Aditya Pandey <103255068+pandeyaditya0002@users.noreply.github.com>
2025-02-28 12:12:05 +0530

    ..

--- Advanced/Expense_Tracker.py
+++ Advanced/Expense_Tracker.py
@@ -6,3 +6,8 @@ def load_expenses():
         with open("expenses.json", "r") as file:
             return json.load(file)
     return []
+
+
+def save_expenses(expenses):
+    with open("expenses.json", "w") as file:
+        json.dump(expenses, file, indent=4)
