0bd2bd384e0585e897eb44d84ccfb17a762f4779
Aditya Pandey <103255068+pandeyaditya0002@users.noreply.github.com>
2025-03-13 17:04:52 +0530

    Refactor game setup and add score updating functionality in Pong game

--- Advanced/pong_game.py
+++ Advanced/pong_game.py
@@ -1,90 +1,131 @@
 import turtle
-import os
 
-# Initialize Turtle Screen
+
+def update_score(l_score, r_score, player, score_board):
+   if player == 'l':
+       l_score += 1
+   else:
+       r_score += 1
+
+   score_board.clear()
+   score_board.write('Left Player: {} -- Right Player: {}'.format(
+       l_score, r_score), align='center',
+       font=('Arial', 24, 'normal'))
+   return l_score, r_score, score_board
+
+
 def setup_game():
-    screen = turtle.Screen()
-    screen.title('Pong Arcade Game')  # Set window title
-    screen.bgcolor('white')  # Set background color
-    screen.setup(width=1000, height=600)  # Set window dimensions
+   screen = turtle.Screen()
+   screen.title('Pong Arcade Game')
+   screen.bgcolor('white')
+   screen.setup(width=1000, height=600)
 
-# Define paddles
-    l_paddle = turtle.Turtle()
-    l_paddle.speed(0)
-    l_paddle.shape('square')
-    l_paddle.color('red')
-    l_paddle.shapesize(stretch_wid=6, stretch_len=2)
-    l_paddle.penup()
-    l_paddle.goto(-400, 0)
+   l_paddle = turtle.Turtle()
+   l_paddle.speed(0)
+   l_paddle.shape('square')
+   l_paddle.color('red')
+   l_paddle.shapesize(stretch_wid=6, stretch_len=2)
+   l_paddle.penup()
+   l_paddle.goto(-400, 0)
 
-    r_paddle = turtle.Turtle()
-    r_paddle.speed(0)
-    r_paddle.shape('square')
-    r_paddle.color('black')
-    r_paddle.shapesize(stretch_wid=6, stretch_len=2)
-    r_paddle.penup()
-    r_paddle.goto(400, 0)
+   r_paddle = turtle.Turtle()
+   r_paddle.speed(0)
+   r_paddle.shape('square')
+   r_paddle.color('black')
+   r_paddle.shapesize(stretch_wid=6, stretch_len=2)
+   r_paddle.penup()
+   r_paddle.goto(400, 0)
 
-  # Define ball
-    ball = turtle.Turtle()
-    ball.speed(40)
-    ball.shape('circle')
-    ball.color('blue')
-    ball.penup()
-    ball.goto(0, 0)
-    ball.dx = 5
-    ball.dy = -5
+   ball = turtle.Turtle()
+   ball.speed(40)
+   ball.shape('circle')
+   ball.color('blue')
+   ball.penup()
+   ball.goto(0, 0)
+   ball.dx = 5
+   ball.dy = -5
 
-     # Define scoreboard
-    score_board = turtle.Turtle()
-    score_board.speed(0)
-    score_board.color('blue')
-    score_board.penup()
-    score_board.hideturtle()
-    score_board.goto(0, 260)
-    score_board.write('Left Player: 0 -- Right Player: 0', align='center', font=('Arial', 24, 'normal'))
+   score_board = turtle.Turtle()
+   score_board.speed(0)
+   score_board.color('blue')
+   score_board.penup()
+   score_board.hideturtle()
+   score_board.goto(0, 260)
+   score_board.write('Left Player: 0 -- Right Player: 0',
+                     align='center', font=('Arial', 24, 'normal'))
 
-    return screen, ball, l_paddle, r_paddle, score_board
+   return screen, ball, l_paddle, r_paddle, score_board
+
 
 def pong_game():
-    game_components = setup_game()
-    screen, ball, l_paddle, r_paddle, score_board = game_components
-    l_score, r_score = 0, 0
- # Define movement functions
-    def l_paddle_up():
-        l_paddle.sety(l_paddle.ycor() + 20)
+   game_components = setup_game()
+   screen = game_components[0]
+   ball = game_components[1]
+   l_paddle = game_components[2]
+   r_paddle = game_components[3]
+   score_board = game_components[4]
+   l_score = 0
+   r_score = 0
 
-    def l_paddle_down():
-        l_paddle.sety(l_paddle.ycor() - 20)
+   def l_paddle_up():
+       l_paddle.sety(l_paddle.ycor() + 20)
 
-    def r_paddle_up():
-        r_paddle.sety(r_paddle.ycor() + 20)
+   def l_paddle_down():
+       l_paddle.sety(l_paddle.ycor() - 20)
 
-    def r_paddle_down():
-        r_paddle.sety(r_paddle.ycor() - 20)
+   def r_paddle_up():
+       r_paddle.sety(r_paddle.ycor() + 20)
 
-         # Map keyboard inputs to paddle movement
-    screen.listen()
-    screen.onkeypress(l_paddle_up, 'e')  # Left paddle moves up with 'e'
-    screen.onkeypress(l_paddle_down, 'x')  # Left paddle moves down with 'x'
-    screen.onkeypress(r_paddle_up, 'Up')  # Right paddle moves up with 'Up Arrow'
-    screen.onkeypress(r_paddle_down, 'Down')  # Right paddle moves down with 'Down Arrow'
+   def r_paddle_down():
+       r_paddle.sety(r_paddle.ycor() - 20)
 
-    while True:
-        screen.update()
-        ball.setx(ball.xcor() + ball.dx)
-        ball.sety(ball.ycor() + ball.dy)
+   screen.listen()
+   screen.onkeypress(l_paddle_up, 'e')
+   screen.onkeypress(l_paddle_down, 'x')
+   screen.onkeypress(r_paddle_up, 'Up')
+   screen.onkeypress(r_paddle_down, 'Down')
 
-        if ball.ycor() > 280 or ball.ycor() < -280:
-            ball.dy *= -1
+   while True:
+       screen.update()
+       ball.setx(ball.xcor()+ball.dx)
+       ball.sety(ball.ycor()+ball.dy)
 
-        if ball.xcor() > 500 or ball.xcor() < -500:
-            ball.goto(0, 0)
-            ball.dy *= -1
+       if ball.ycor() > 280:
+           ball.sety(280)
+           ball.dy *= -1
 
-        if ((ball.xcor() > 360 and ball.xcor() < 370 and ball.ycor() < r_paddle.ycor() + 40 and ball.ycor() > r_paddle.ycor() - 40) or
-            (ball.xcor() < -360 and ball.xcor() > -370 and ball.ycor() < l_paddle.ycor() + 40 and ball.ycor() > l_paddle.ycor() - 40)):
-            ball.dx *= -1
+       if ball.ycor() < -280:
+           ball.sety(-280)
+           ball.dy *= -1
 
-        if __name__ == "__main__":
-    pong_game()
+       if ball.xcor() > 500:
+           ball.goto(0, 0)
+           ball.dy *= -1
+           l_score, r_score, score_board = update_score(
+               l_score, r_score, 'l', score_board)
+           continue
+
+       elif ball.xcor() < -500:
+           ball.goto(0, 0)
+           ball.dy *= -1
+           l_score, r_score, score_board = update_score(
+               l_score, r_score, 'r', score_board)
+           continue
+
+       if ((ball.xcor() > 360) and
+           (ball.xcor() < 370) and
+           (ball.ycor() < r_paddle.ycor()+40) and
+               (ball.ycor() > r_paddle.ycor()-40)):
+           ball.setx(360)
+           ball.dx *= -1
+
+       if ((ball.xcor() < -360) and
+               (ball.xcor() > -370) and
+               (ball.ycor() < l_paddle.ycor()+40) and
+               (ball.ycor() > l_paddle.ycor()-40)):
+           ball.setx(-360)
+           ball.dx *= -1
+
+
+if __name__ == '__main__':
+   pong_game()
