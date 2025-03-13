279c91f2cadb0afb422d5acdcf6b18c0b1419ea3
Aditya Pandey <103255068+pandeyaditya0002@users.noreply.github.com>
2025-03-13 17:01:23 +0530

    Add paddles to the Pong game setup

--- Advanced/pong_game.py
+++ Advanced/pong_game.py
@@ -7,3 +7,20 @@ def setup_game():
     screen.title('Pong Arcade Game')  # Set window title
     screen.bgcolor('white')  # Set background color
     screen.setup(width=1000, height=600)  # Set window dimensions
+
+# Define paddles
+    l_paddle = turtle.Turtle()
+    l_paddle.speed(0)
+    l_paddle.shape('square')
+    l_paddle.color('red')
+    l_paddle.shapesize(stretch_wid=6, stretch_len=2)
+    l_paddle.penup()
+    l_paddle.goto(-400, 0)
+
+    r_paddle = turtle.Turtle()
+    r_paddle.speed(0)
+    r_paddle.shape('square')
+    r_paddle.color('black')
+    r_paddle.shapesize(stretch_wid=6, stretch_len=2)
+    r_paddle.penup()
+    r_paddle.goto(400, 0)
